694b207eabd63aef48e822e466841e4369e561f6
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2024-05-27 14:38:07 +0300

    заменены методы ввода-вывода в потоки

--- tcpip/c_echo_thread.cc
+++ tcpip/c_echo_thread.cc
@@ -4,13 +4,13 @@
 //
 #include "tcpip.hh"
 //------------------------------------------------------------------------------
+#include <cstdlib>
+#include <cstdint>
 #include <vector>
 #include <iostream>
 #include <string>
-#include <cstdint>
 #include <ctime>
 #include <cstring>
-#include <cstdlib>
 //------------------------------------------------------------------------------
 using SA = struct sockaddr;
 //------------------------------------------------------------------------------
@@ -19,19 +19,20 @@ static std::int32_t sockfd;
 //------------------------------------------------------------------------------
 void* copyto(void* arg)
    // функция потока
+   // отвечает за ввод текста и отправку серверу введенного текста
 {
    char sendline[pl::mr::MAXLINE];
    std::memset(&sendline[0],0,sizeof(sendline));
-   while (fgets(sendline,pl::mr::MAXLINE,stdin)!=NULL) 
+   while (tcp.tcp_fgets(sendline,pl::mr::MAXLINE,stdin)!=NULL)  
       tcp.tcp_writen(sockfd,sendline,std::strlen(sendline));
    tcp.tcp_shutdown(sockfd,SHUT_WR);
    return NULL;
 }
 ////////////////////////////////////////////////////////////////////////////////
-int main(int argc, char** argv)
+std::int32_t main(int argc, char** argv)
 {
    std::vector<std::string> args(argv,argv+argc);
-   if (args.size()!=2) std::cerr << "W: Usage: a.out <IPaddress>\n";
+   if (args.size()!=2) std::cerr << "W: usage: a.out <IPaddress>\n";
    else {
       try {
          struct sockaddr_in addr;
@@ -44,19 +45,23 @@ int main(int argc, char** argv)
          std::string s = args.at(1);
          if (inet_pton(AF_INET,s.c_str(),&addr.sin_addr)<=0) {
             std::cout << "E: inet_pton error for " << s << '\n';
-            return 0;
+            std::exit(EXIT_SUCCESS);
          }
 
          tcp.tcp_connect(sockfd,(SA*)&addr,sizeof(addr));
+         std::cout << "W: connection to server...\n";
 
          char recvline[pl::mr::MAXLINE];
          std::memset(&recvline[0],0,sizeof(recvline));
          pthread_t tid;
 
+         // создаем поток для ввода данных и их отправки серверу
          tcp.tcp_pthread_create(&tid,NULL,::copyto,NULL);
 
+         // основной поток занимается чтением поступающих данных
+         // от сервера и выводом их в терминал
          while (tcp.tcp_readline(sockfd,recvline,pl::mr::MAXLINE)>0) {
-            std::cout << recvline;
+            tcp.tcp_fputs(recvline,stdout);
             std::memset(&recvline[0],0,sizeof(recvline));
          }
       }
@@ -64,6 +69,5 @@ int main(int argc, char** argv)
          std::cout << ex.what() << '\n';
       }
    }
-   return 0;
+   std::exit(EXIT_SUCCESS);
 }
-
