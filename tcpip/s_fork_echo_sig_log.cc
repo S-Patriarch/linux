8f3dcb7c118383d34284e1803b6560003fde4153
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2024-06-08 00:30:20 +0300

    параллельный эхо-сервер с дополнительным потоком, отписывающим log-файл

---
+++ tcpip/s_fork_echo_sig_log.cc
@@ -0,0 +1,142 @@
+//
+// (c) 2024 S-Patriarch
+// Параллельный эхо-сервер с уничтожением зомби процессов.
+//
+#include "tcpip.hh"
+#include <unistd.h>
+//------------------------------------------------------------------------------
+#ifndef PL_LOGGER_HH
+#include "pl/logger.hh"
+#endif
+//------------------------------------------------------------------------------
+#include <iostream>
+#include <cstdint>
+#include <ctime>
+#include <cstring>
+#include <cstdlib>
+#include <sys/wait.h>
+#include <thread>
+#include <functional>
+#include <sstream>
+//------------------------------------------------------------------------------
+using SA = struct sockaddr;
+//------------------------------------------------------------------------------
+static bool t_shouldstop {false}; // флаг остановки потока
+static std::string FILE_LOGGER {"slog"};
+//------------------------------------------------------------------------------
+static struct {
+   std::string _strlogger {""}; // строка записи
+   bool _iswritelogger {false}; // флаг возможности записи
+} log;
+//------------------------------------------------------------------------------
+void sig_chld(std::int32_t signo)
+   // функция, корректно обрабатывающая сигнал SIGCHLD
+{
+   std::int32_t stat {};
+   pid_t pid {};
+   // вызываем функцию waitpid (обработчик завершения дочернего процесса)
+   // в цикле, получая состояние любого из дочерних процессов, которые 
+   // завершились
+   // WNOHANG - параметр, указывающий функции waitpid, что не нужно
+   // блокироваться, если существует выполняемые дочерние процессы,
+   // которые еще не завершились
+   while ((pid = waitpid(-1,&stat,WNOHANG))>0) {
+      std::cout << "W: child " << pid << " terminated\n";
+   }
+}
+//------------------------------------------------------------------------------
+void* write_to_log_file(void* arg) 
+   // функция потока, которая асинхронно основному потоку будет 
+   // отписывать информацию в log-файл
+{ 
+   pl::Logger l(::FILE_LOGGER);
+   for (;;) {
+   // проверяем на необходимость остановки потока
+      if (::t_shouldstop) break;
+      else {
+         // проверяем на необходимость записи
+         if (log._iswritelogger) {
+            l.write(log._strlogger);
+            log._strlogger = "";
+            log._iswritelogger = false;
+         }
+      }
+   }
+   return NULL;
+}
+////////////////////////////////////////////////////////////////////////////////
+std::int32_t main(std::int32_t argc, char** argv)
+{
+   pl::TCPip tcp;
+   
+   pthread_t tid;
+   tcp.tcp_pthread_create(&tid,NULL,::write_to_log_file,NULL);
+
+   try {
+      struct sockaddr_in saddr;
+      struct sockaddr_in caddr;
+ 
+      std::int32_t listenfd = tcp.tcp_socket(AF_INET,SOCK_STREAM,0);
+
+      std::memset(&saddr,0,sizeof(saddr));
+      saddr.sin_family = AF_INET;
+      saddr.sin_port = htons(pl::mr::SERV_PORT);
+      saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+      tcp.tcp_bind(listenfd,(SA*)&saddr,sizeof(saddr));
+      tcp.tcp_listen(listenfd,pl::mr::LISTENQ);
+
+      tcp.tcp_signal(SIGCHLD,sig_chld);
+
+      std::cout << "W: the server is running...\n";
+
+      char buff[pl::mr::MAXLINE];
+      for (;;) {
+         std::int32_t connfd {};
+         socklen_t len = sizeof(caddr);
+         if ((connfd = accept(listenfd,(SA*)&caddr,&len))<0) {
+            if (errno==EINTR) continue; 
+            else {
+               char errmsg[pl::mr::MAXLINE];
+               strcpy(errmsg,"E: accept error - ");
+               char* s = std::strerror(errno);
+               throw pl::Exception(strcat(errmsg,s));
+            }
+         }
+         std::cout << "connection from " 
+                   << inet_ntop(AF_INET,&caddr.sin_addr,buff,sizeof(buff)) 
+                   << ':' << ntohs(caddr.sin_port) << '\n';
+         std::ostringstream ss;
+         ss << ntohs(caddr.sin_port);
+         log._strlogger = "connection from "+
+            (std::string)inet_ntop(AF_INET,&caddr.sin_addr,buff,sizeof(buff))+
+            ":"+ss.str()+"\n";
+         log._iswritelogger = true;
+
+         pid_t pid {};
+         if ((pid = tcp.tcp_fork())==0) {
+            tcp.tcp_close(listenfd); 
+            std::int32_t n {};  
+            char recvline[pl::mr::MAXLINE];
+            for (;;) {
+               std::memset(&recvline[0],0,sizeof(recvline));
+               if ((n = tcp.tcp_read(connfd,recvline,pl::mr::MAXLINE))>0)
+                  tcp.tcp_write(connfd,recvline,n);
+               else break;
+            }
+            tcp.tcp_close(connfd); 
+            std::exit(EXIT_SUCCESS);
+         }
+
+         tcp.tcp_close(connfd); 
+      }
+   }
+   catch (std::exception& ex) {
+      std::cout << ex.what() << '\n';
+   }
+   
+   ::t_shouldstop = true;
+   tcp.tcp_pthread_join(tid,NULL);
+   std::exit(EXIT_SUCCESS);
+}
+
