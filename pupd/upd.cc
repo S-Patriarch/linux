d29ca939029724c08ee9862d341c7b06c6531a26
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2024-07-27 11:02:33 +0300

    оптимизация кода

--- pupd/upd.cc
+++ pupd/upd.cc
@@ -1,47 +1,51 @@
-//
 // (c) 2024 S-Patriarch
 // Обновление системы GNU/Linux семейства Debian Linux.
-//
+
 #include <cstdlib>
-#include <cstdint>
 #include <vector>
 #include <iostream>
 ////////////////////////////////////////////////////////////////////////////////
-std::int32_t main(int argc, char** argv)
+int main(int argc, char** argv)
 {
-   std::vector<std::string> args(argv,argv+argc);
+   using std::string;
+   using std::system;
+   using std::cout;
+   using std::endl;
+
+   std::vector<string> args(argv,argv+argc);
    
    if (args.size()==2) {
-      std::string opt {args.at(1)};
+      string opt {args.at(1)};
       if (opt=="-f" || opt=="--full") {
-         std::system("sudo apt update && sudo apt full-upgrade -y");
-         std::system("sudo apt autoremove -y");
-         std::system("sudo apt autoclean -y");
-         std::system("sudo apt clean -y");
+         system("sudo apt update && sudo apt full-upgrade -y");
+         system("sudo apt autoremove -y");
+         system("sudo apt autoclean -y");
+         system("sudo apt clean -y");
       }
       else if (opt=="-u" || opt=="--updating") {
-         std::system("sudo apt update && sudo apt full-upgrade -y");
+         system("sudo apt update && sudo apt full-upgrade -y");
       }
       else if (opt=="-c" || opt=="--cleaning") {
-         std::system("sudo apt autoremove -y");
-         std::system("sudo apt autoclean -y");
-         std::system("sudo apt clean -y");
+         system("sudo apt autoremove -y");
+         system("sudo apt autoclean -y");
+         system("sudo apt clean -y");
       }
       else {
-         std::cout << "E: Command line option [" << opt << "] unclear.\n\n";
+         cout << "E: command line option [" << opt << "] unclear\n" << endl;
       }
    }
    else if (args.size()!=2) {
-      std::cout << "Usage: upd [<options>]\n\n"
-                << "upd is a assistand for updating and cleaning the system "
-                << "of the family Debian Linux\n"
-                << "for people with very little experience in OS GNU/Linux.\n\n"
-                << "Options:\n"
-                << "  -f  --full      Updating the system to the latest "
-                << "version followed by cleaning.\n"
-                << "  -u  --updating  Updating the system to the latest "
-                << "version.\n"
-                << "  -c  --cleaning  System cleaning.\n\n";
+      cout << "Usage: upd [<options>]\n\n"
+           << "upd is a assistand for updating and cleaning the system "
+           << "of the family Debian Linux\n"
+           << "for people with very little experience in OS GNU/Linux.\n\n"
+           << "Options:\n"
+           << "  -f  --full      Updating the system to the latest "
+           << "version followed by cleaning.\n"
+           << "  -u  --updating  Updating the system to the latest "
+           << "version.\n"
+           << "  -c  --cleaning  System cleaning.\n"
+           << endl;
    }
    std::exit(EXIT_SUCCESS);
 }
