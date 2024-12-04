599e4635a7a590136e253978ace907d122616a28
S-Patriarch <134074106+S-Patriarch@users.noreply.github.com>
2024-12-04 21:42:38 +0300

    Add files via upload

--- pupd/upd_v4/command_generator.cc
+++ pupd/upd_v4/command_generator.cc
@@ -8,7 +8,8 @@ namespace upd {
 
     if (m_packageManager=="apt") {
       if (option=="-f" || option=="--full") {
-        commands.emplace_back("sudo apt update && sudo apt full-upgrade -y");
+        commands.emplace_back("sudo apt update");
+        commands.emplace_back("sudo apt full-upgrade -y");
         commands.emplace_back("sudo apt autoremove -y");
         commands.emplace_back("sudo apt autoclean -y");
         commands.emplace_back("sudo apt clean -y");
@@ -19,17 +20,56 @@ namespace upd {
         commands.emplace_back("sudo apt clean -y");
       }
       if (option=="-u" || option=="--update") {
-        commands.emplace_back("sudo apt update && sudo apt full-upgrade -y");
+        commands.emplace_back("sudo apt update");
+        commands.emplace_back("sudo apt full-upgrade -y");
       }
     }
 
     if (m_packageManager=="dnf") {
+      if (option=="-f" || option=="--full") {
+        commands.emplace_back("sudo dnf check-update");
+        commands.emplace_back("sudo dnf upgrade --refresh -y");
+        commands.emplace_back("sudo dnf autoremove -y");
+        commands.emplace_back("sudo dnf clean all -y");
+      }
+      if (option=="-c" || option=="--clean") {
+        commands.emplace_back("sudo dnf autoremove -y");
+        commands.emplace_back("sudo dnf clean all -y");
+      }
+      if (option=="-u" || option=="--update") {
+        commands.emplace_back("sudo dnf check-update");
+        commands.emplace_back("sudo dnf upgrade --refresh -y");
+      }
     }
 
     if (m_packageManager=="pacman") {
+      if (option=="-f" || option=="--full") {
+        commands.emplace_back("sudo pacman -Syu --noconfirm");
+        commands.emplace_back("sudo pacman -Rns --noconfirm");
+        commands.emplace_back("sudo pacman -Sc --noconfirm");
+      }
+      if (option=="-c" || option=="--clean") {
+        commands.emplace_back("sudo pacman -Rns --noconfirm");
+        commands.emplace_back("sudo pacman -Sc --noconfirm");
+      }
+      if (option=="-u" || option=="--update") {
+        commands.emplace_back("sudo pacman -Syu --noconfirm");
+      }
     }
 
     if (m_packageManager=="zypper") {
+      if (option=="-f" || option=="--full") {
+        commands.emplace_back("sudo zypper refresh");
+        commands.emplace_back("sudo zypper update -y");
+        commands.emplace_back("sudo zypper clean --all");
+      }
+      if (option=="-c" || option=="--clean") {
+        commands.emplace_back("sudo zypper clean --all");
+      }
+      if (option=="-u" || option=="--update") {
+        commands.emplace_back("sudo zypper refresh");
+        commands.emplace_back("sudo zypper update -y");
+      }
     }
 
     return commands;
